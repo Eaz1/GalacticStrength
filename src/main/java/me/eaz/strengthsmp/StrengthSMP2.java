5d6256645e3dc1530bcf739ec4821ccd6d1ece2a
Eaz1 <yeabruh652@gmail.com>
2026-07-09 22:55:03 -0700

    Implement StrengthSMP main plugin class

---
+++ src/main/java/me/eaz/strengthsmp/StrengthSMP2.java
@@ -0,0 +1,59 @@
+package me.eaz.strengthsmp;
+
+import me.eaz.strengthsmp.commands.StrengthCommand;
+import me.eaz.strengthsmp.listeners.DamageListener;
+import me.eaz.strengthsmp.listeners.DeathListener;
+import me.eaz.strengthsmp.managers.StrengthManager;
+import me.eaz.strengthsmp.utils.Messages;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StrengthSMP extends JavaPlugin {
+
+    private static StrengthSMP instance;
+    private StrengthManager strengthManager;
+    private Messages messages;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        saveDefaultConfig();
+
+        messages = new Messages(this);
+        strengthManager = new StrengthManager(this);
+
+        getServer().getPluginManager().registerEvents(
+                new DeathListener(this), this);
+
+        getServer().getPluginManager().registerEvents(
+                new DamageListener(this), this);
+
+        getCommand("strength").setExecutor(
+                new StrengthCommand(this));
+
+        getLogger().info("StrengthSMP enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+
+        if (strengthManager != null) {
+            strengthManager.saveData();
+        }
+
+        getLogger().info("StrengthSMP disabled!");
+    }
+
+    public static StrengthSMP getInstance() {
+        return instance;
+    }
+
+    public StrengthManager getStrengthManager() {
+        return strengthManager;
+    }
+
+    public Messages getMessages() {
+        return messages;
+    }
+}
